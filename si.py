7a388513083a65ce13b39be3ffef30ba5fcacd6b
Tony Su <peitaosu@163.com>
2020-10-20 22:21:09 +0800

    fix

--- si.py
+++ si.py
@@ -1,5 +1,5 @@
 import sys, subprocess
 
 def ensure_import(module_name)
-subprocess.run([sys.executable, "-m", "ensurepip"])
-subprocess.run([sys.executable, "-m", "pip", "install", module_name])
+    subprocess.run([sys.executable, "-m", "ensurepip"])
+    subprocess.run([sys.executable, "-m", "pip", "install", module_name])
