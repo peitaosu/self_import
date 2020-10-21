65ff0e3b76c26e37181ce3209659709b5e0ad068
Tony Su <peitaosu@163.com>
2020-10-21 22:03:22 +0800

    try import before install

--- si.py
+++ si.py
@@ -1,5 +1,9 @@
 import sys, subprocess
 
 def ensure_import(module_name)
-    subprocess.run([sys.executable, "-m", "ensurepip"])
-    subprocess.run([sys.executable, "-m", "pip", "install", module_name])
+    try:
+        __import__(module_name)
+    except:
+        subprocess.run([sys.executable, "-m", "ensurepip"])
+        subprocess.run([sys.executable, "-m", "pip", "install", module_name])
+        __import__(module_name)
