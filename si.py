3a65166892cbb5a3acef823dcbdc2eff2f219127
Tony Su <peitaosu@163.com>
2020-10-22 18:16:41 +0800

    support append path

--- si.py
+++ si.py
@@ -7,3 +7,6 @@ def ensure_import(module_name)
         subprocess.run([sys.executable, "-m", "ensurepip"])
         subprocess.run([sys.executable, "-m", "pip", "install", module_name])
         __import__(module_name)
+
+def update_path():
+    sys.path.append([sys.executable, "Lib", "site-packages"])
