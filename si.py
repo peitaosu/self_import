5d66e4ac330a05091fbdf84945a45b44b2b363f7
Tony Su <peitaosu@163.com>
2020-10-24 22:32:53 +0800

    use check_call instead of run

--- si.py
+++ si.py
@@ -4,8 +4,8 @@ def ensure_import(module_name)
     try:
         __import__(module_name)
     except:
-        subprocess.run([sys.executable, "-m", "ensurepip"])
-        subprocess.run([sys.executable, "-m", "pip", "install", module_name])
+        subprocess.check_call([sys.executable, "-m", "ensurepip"])
+        subprocess.check_call([sys.executable, "-m", "pip", "install", module_name])
         __import__(module_name)
 
 def update_path(path = None):
