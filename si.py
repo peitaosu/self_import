995d0976e36d07ed4ba3ee92a5091f6919f508d4
Tony Su <peitaosu@163.com>
2020-10-25 22:45:10 +0800

    install depends from file

--- si.py
+++ si.py
@@ -12,3 +12,7 @@ def update_path(path = None):
     sys.path.append([sys.executable, "Lib", "site-packages"])
     if path:
         sys.path.append(path)
+
+def install_dependencies(dep_file):
+    subprocess.check_call([sys.executable, "-m", "ensurepip"])
+    subprocess.check_call([sys.executable, "-m", "pip", "install", "-r", dep_file])
