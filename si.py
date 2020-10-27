e67933c321f1304036d8744e53c6a2e776fe52a5
Tony Su <peitaosu@163.com>
2020-10-27 21:47:40 +0800

    import and return

--- si.py
+++ si.py
@@ -2,11 +2,11 @@ import sys, subprocess
 
 def ensure_import(module_name):
     try:
-        __import__(module_name)
+        return __import__(module_name)
     except:
         subprocess.check_call([sys.executable, "-m", "ensurepip"])
         subprocess.check_call([sys.executable, "-m", "pip", "install", module_name])
-        __import__(module_name)
+        return  __import__(module_name)
 
 def update_path(path = None):
     sys.path.append([sys.executable, "Lib", "site-packages"])
