b0fb0f1197139e1b6ebe559b47fedf9dac5b30bf
Tony Su <peitaosu@163.com>
2020-11-02 19:41:44 +0800

    support install to dest path

--- self_import.py
+++ self_import.py
@@ -1,18 +1,23 @@
 import sys, subprocess
 
-def ensure_import(module_name):
+def ensure_import(module_name, dest_path=None):
     try:
         return __import__(module_name)
     except:
         subprocess.check_call([sys.executable, "-m", "ensurepip"])
-        subprocess.check_call([sys.executable, "-m", "pip", "install", module_name])
+        if dest_path:
+            subprocess.check_call([sys.executable, "-m", "pip", "install", module_name, "-t", dest_path])
+        else:
+            subprocess.check_call([sys.executable, "-m", "pip", "install", module_name])
         return  __import__(module_name)
 
 def update_lib_path(path = None):
-    sys.path.append([sys.executable, "Lib", "site-packages"])
     if path:
         sys.path.append(path)
 
-def install_dependencies(dep_file):
+def install_dependencies(dep_file, dest_path=None):
     subprocess.check_call([sys.executable, "-m", "ensurepip"])
-    subprocess.check_call([sys.executable, "-m", "pip", "install", "-r", dep_file])
+    if dest_path:
+        subprocess.check_call([sys.executable, "-m", "pip", "install", "-r", dep_file, "-t", dest_path])
+    else:
+        subprocess.check_call([sys.executable, "-m", "pip", "install", "-r", dep_file])
