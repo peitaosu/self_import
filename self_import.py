5171a561ea77a5be39c2c6888d4c58f477a91310
Tony Su <peitaosu@163.com>
2020-11-07 22:33:33 +0800

    combine functions

--- self_import.py
+++ self_import.py
@@ -1,21 +1,25 @@
-import sys, subprocess
+import os, sys, subprocess
 
 def ensure_import(module_name, dest_path=None):
     try:
         return __import__(module_name)
     except:
-        subprocess.check_call([sys.executable, "-m", "ensurepip"])
-        if dest_path:
-            subprocess.check_call([sys.executable, "-m", "pip", "install", module_name, "-t", dest_path])
-            sys.append(dest_path)
-        else:
-            subprocess.check_call([sys.executable, "-m", "pip", "install", module_name])
+        install_module(["-m", "pip", "install", module_name], dest_path)
         return  __import__(module_name)
 
 def install_dependencies(dep_file, dest_path=None):
-    subprocess.check_call([sys.executable, "-m", "ensurepip"])
-    if dest_path:
-        subprocess.check_call([sys.executable, "-m", "pip", "install", "-r", dep_file, "-t", dest_path])
-        sys.append(dest_path)
-    else:
-        subprocess.check_call([sys.executable, "-m", "pip", "install", "-r", dep_file])
+    install_module(["-m", "pip", "install", "-r", dep_file], dest_path)
+
+def install_module(command, dest_path=None):
+    try:
+        subprocess.check_call([sys.executable, "-m", "ensurepip"])
+        exe = [sys.executable]
+        exe_cmd = exe.extend(command)
+        if dest_path:
+            subprocess.check_call(exe_cmd.extend(["-t", dest_path]))
+            sys.append(dest_path)
+            os.environ["PYTHONPATH"] = dest_path + (";" + os.environ["PYTHONPATH"] if "PYTHONPATH" in os.environ else "")
+        else:
+            subprocess.check_call(exe_cmd)
+    except subprocess.CalledProcessError(e):
+        print(e.output)
